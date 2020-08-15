2022069ffd15581ba961b6ddd9ef22513373d598
libbyangel <1079719175@qq.com>
2020-08-15 12:18:29 +0800

    add assert

--- src/main/java/TestJunit.java
+++ src/main/java/TestJunit.java
@@ -5,11 +5,10 @@
 
 import Jama.Matrix;
 import org.junit.Test;
-import org.mockito.Mock;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 //测试矩阵加法和矩阵减法
 public class TestJunit {
@@ -29,29 +28,30 @@ public class TestJunit {
 
     // 单元测试和 mockito 的使用
     @Test
-    public void testMockMinusMatrix(){
+    public void testMockMinusMatrix() {
         // 实例化两个 mock 对象，但我们不知道 Matrix 里面的方法 minus 是怎么实现的
         // 所以插桩，不管它内部实现是什么，只要运行了这个函数，我们就返回一个预设的矩阵
         Matrix A = mock(Matrix.class);
         Matrix B = mock(Matrix.class);
 
         Matrix result = new Matrix(m1);
-        when (A.minus(B)).thenReturn(result);       // minus 输入矩阵，输出矩阵
+        when(A.minus(B)).thenReturn(result);       // minus 输入矩阵，输出矩阵
 
-        for (int i=0; i<result.getRowDimension();i++){
-            for (int j=0; j<result.getColumnDimension();j++){
-                System.out.print((A.minus(B)).get(i,j) + "   ");
+        for (int i = 0; i < result.getRowDimension(); i++) {
+            for (int j = 0; j < result.getColumnDimension(); j++) {
+                assertEquals((Double) result.get(i, j), (Double) (A.minus(B)).get(i, j));
             }
-            System.out.println();
         }
     }
+
     @Test
-    public void testMockMyMinus(){
+    public void testMockMyMinus() {
         //这个方法用来测试一个自己写的类 MessageUtil 和方法 myMinusMock
         this.messageUtil = mock(MessageUtil.class);   //输入为矩阵，输出 boolean
-        when (this.messageUtil.myMinusMock(this.M1,this.M2)).thenReturn(true);
+        when(this.messageUtil.myMinusMock(this.M1, this.M2)).thenReturn(true);
 
-        System.out.println(this.messageUtil.myMinusMock(this.M1,this.M2));
+        assertEquals(true, this.messageUtil.myMinusMock(this.M1, this.M2));
+//        System.out.println(this.messageUtil.myMinusMock(this.M1,this.M2));
     }
 
     // 简单单元测试
@@ -59,16 +59,16 @@ public class TestJunit {
     public void testAddMatrix() {
         double[][] test = {
                 {5, 5, 5},
-                {7, 7, 6},
+                {7, 7, 7},
                 {9, 9, 9},
         };
         Matrix testMatrix = new Matrix(test);
         this.messageUtil = new MessageUtil(this.M1, this.M2);
         Matrix resultMatrix = this.messageUtil.addMatrix();
         // 注意这里一定要强制转换，不然的话没办法进行比较 (另外，就算用循环输出的也只是一个结果)
-        for(int i=0; i<testMatrix.getRowDimension();i++){
-            for (int j=0; j<testMatrix.getColumnDimension();j++){
-                assertEquals((Double)testMatrix.get(i,j), (Double) resultMatrix.get(i,j));
+        for (int i = 0; i < testMatrix.getRowDimension(); i++) {
+            for (int j = 0; j < testMatrix.getColumnDimension(); j++) {
+                assertEquals((Double) testMatrix.get(i, j), (Double) resultMatrix.get(i, j));
             }
         }
     }
@@ -83,6 +83,6 @@ public class TestJunit {
         Matrix testMatrix = new Matrix(test);
         this.messageUtil = new MessageUtil(this.M1, this.M2);
         Matrix resultMatrix = this.messageUtil.minusMatrix();
-        assertEquals((Double)testMatrix.get(0,0), (Double) resultMatrix.get(0,0));
+        assertEquals((Double) testMatrix.get(0, 0), (Double) resultMatrix.get(0, 0));
     }
 }
--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -5,15 +5,18 @@
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
 public class TestRunner {
     public static void main(String[] args) throws Throwable {
         Result result = JUnitCore.runClasses(TestJunit.class);
-        for (Failure failure : result.getFailures()) {
-            throw failure.getException();
-//            System.out.println(failure.toString());         //会返回所有 test 运行之后的一个与运算，只要有一个不对就是 false
-        }
-        System.out.println(result.wasSuccessful());
+//        for (Failure failure : result.getFailures()) {
+//            throw failure.getException();
+////            System.out.println(failure.toString());         //会返回所有 test 运行之后的一个与运算，只要有一个不对就是 false
+//        }
+
+        assert (result.wasSuccessful());
+
+        System.out.println("RunCount: " + result.getRunCount() + ", RunTime: " + result.getRunTime());
+//        System.out.println(result.wasSuccessful());
     }
 }
