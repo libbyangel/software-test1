f2ce2286eade48d88ae1b71b9d31f7499e2a9759
libbyangel <1079719175@qq.com>
2020-08-15 12:53:41 +0800

    update

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -5,14 +5,15 @@
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class TestRunner {
     public static void main(String[] args) throws Throwable {
         Result result = JUnitCore.runClasses(TestJunit.class);
-//        for (Failure failure : result.getFailures()) {
-//            throw failure.getException();
-////            System.out.println(failure.toString());         //会返回所有 test 运行之后的一个与运算，只要有一个不对就是 false
-//        }
+        for (Failure failure : result.getFailures()) {
+            throw failure.getException();
+//            System.out.println(failure.toString());         //会返回所有 test 运行之后的一个与运算，只要有一个不对就是 false
+        }
 
         assert (result.wasSuccessful());
 
