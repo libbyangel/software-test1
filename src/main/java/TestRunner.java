003451b847659dbaeb18b9b09079951a8152761a
libbyangel <1079719175@qq.com>
2020-08-23 21:19:24 +0800

    上传jar包

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -12,12 +12,8 @@ public class TestRunner {
         Result result = JUnitCore.runClasses(TestJunit.class);
         for (Failure failure : result.getFailures()) {
             throw failure.getException();
-//            System.out.println(failure.toString());         //会返回所有 test 运行之后的一个与运算，只要有一个不对就是 false
         }
-
         assert (result.wasSuccessful());
-
         System.out.println("RunCount: " + result.getRunCount() + ", RunTime: " + result.getRunTime());
-//        System.out.println(result.wasSuccessful());
     }
 }
